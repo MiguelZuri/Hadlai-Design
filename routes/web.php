f5fd79fb25f4f30e932811a33aa16a2cde70a5e9
Miguel Zuri <zurijv97@gmail.com>
2019-10-09 15:51:31 -0500

    AD index y Product MVC

--- routes/web.php
+++ routes/web.php
@@ -12,7 +12,11 @@
 */
 
 Route::get('/', function () {
-    return view('welcome');
+    return view('index');
+});
+
+Route::get('/productos', function () {
+    return view('productos');
 });
 
 Auth::routes();
