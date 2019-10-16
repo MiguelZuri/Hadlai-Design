3fe28ea945334519de92ccd4fbe124d241338137
Miguel Zuri <zurijv97@gmail.com>
2019-10-15 20:07:56 -0500

    AD plantilla visual

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -1,18 +1,217 @@
 @extends('layouts.app')
 @section('content')
- 
-<div class="container">
-    <div class="jumbotron">
-        <h1 class="display-4 text-center">Productos</h1>
-        <div class="row text-center">
-            <div class="col-6">
-                <a href="{{route('lista-productos')}}" class="btn btn-primary btn-lg" role="button">Tabla productos</a>
-            </div>
-            <div class="col-6">
-                <a href="{{route('ProductController@create')}}" class="btn btn-secondary btn-lg" role="button">Registro Productos</a>
-            </div>
+
+<div class="col-md-12">
+  <div class="card">
+    <div class="card-header">
+      <div class="tools float-right">
+        <div class="dropdown">
+          <button type="button" class="btn btn-default dropdown-toggle btn-link btn-icon" data-toggle="dropdown">
+            <i class="tim-icons icon-settings-gear-63"></i>
+          </button>
+          <div class="dropdown-menu dropdown-menu-right">
+            <a class="dropdown-item" href="#">Action</a>
+            <a class="dropdown-item" href="#">Another action</a>
+            <a class="dropdown-item" href="#">Something else here</a>
+            <a class="dropdown-item text-danger" href="#">Remove Data</a>
+          </div>
         </div>
+      </div>
+      <h4 class="card-title">Simple Table</h4>
     </div>
+    <div class="card-body">
+      <div class="table-responsive ps">
+        <table class="table">
+          <thead class="text-primary">
+            <tr>
+              <th class="text-center">
+                #
+              </th>
+              <th>
+                Name
+              </th>
+              <th>
+                Job Position
+              </th>
+              <th class="text-center">
+                Since
+              </th>
+              <th class="text-right">
+                Salary
+              </th>
+              <th class="text-right">
+                Actions
+              </th>
+            </tr>
+          </thead>
+          <tbody>
+            <tr>
+              <td class="text-center">
+                <div class="photo">
+                  <img src="https://black-dashboard-pro-laravel.creative-tim.com/black/img/tania.jpg" alt="Table image">
+                </div>
+              </td>
+              <td>
+                Andrew Mike
+              </td>
+              <td>
+                Develop
+              </td>
+              <td class="text-center">
+                2013
+              </td>
+              <td class="text-right">
+                € 99,225
+              </td>
+              <td class="text-right">
+                <button type="button" rel="tooltip" class="btn btn-success btn-link btn-sm btn-icon " data-original-title="Refresh" title="">
+                  <i class="tim-icons icon-refresh-01"></i>
+                </button>
+                <button type="button" rel="tooltip" class="btn btn-danger btn-link btn-sm " data-original-title="Delete" title="">
+                  <i class="tim-icons icon-simple-remove"></i>
+                </button>
+              </td>
+            </tr>
+            <tr>
+              <td class="text-center">
+                <div class="photo">
+                  <img src="https://black-dashboard-pro-laravel.creative-tim.com/black/img/robi.jpg" alt="Table image">
+                </div>
+              </td>
+              <td>
+                John Doe
+              </td>
+              <td>
+                Design
+              </td>
+              <td class="text-center">
+                2012
+              </td>
+              <td class="text-right">
+                € 89,241
+              </td>
+              <td class="text-right">
+                <button type="button" rel="tooltip" class="btn btn-success btn-link btn-sm btn-icon " data-original-title="Refresh" title="">
+                  <i class="tim-icons icon-refresh-01"></i>
+                </button>
+                <button type="button" rel="tooltip" class="btn btn-danger btn-link btn-sm " data-original-title="Delete" title="">
+                  <i class="tim-icons icon-simple-remove"></i>
+                </button>
+              </td>
+            </tr>
+            <tr>
+              <td class="text-center">
+                <div class="photo">
+                  <img src="https://black-dashboard-pro-laravel.creative-tim.com/black/img/lora.jpg" alt="Table image">
+                </div>
+              </td>
+              <td>
+                Alex Mike
+              </td>
+              <td>
+                Design
+              </td>
+              <td class="text-center">
+                2010
+              </td>
+              <td class="text-right">
+                € 92,144
+              </td>
+              <td class="text-right">
+                <button type="button" rel="tooltip" class="btn btn-success btn-link btn-sm btn-icon " data-original-title="Refresh" title="">
+                  <i class="tim-icons icon-refresh-01"></i>
+                </button>
+                <button type="button" rel="tooltip" class="btn btn-danger btn-link btn-sm " data-original-title="Delete" title="">
+                  <i class="tim-icons icon-simple-remove"></i>
+                </button>
+              </td>
+            </tr>
+            <tr>
+              <td class="text-center">
+                <div class="photo">
+                  <img src="https://black-dashboard-pro-laravel.creative-tim.com/black/img/jana.jpg" alt="Table image">
+                </div>
+              </td>
+              <td>
+                Mike Monday
+              </td>
+              <td>
+                Marketing
+              </td>
+              <td class="text-center">
+                2013
+              </td>
+              <td class="text-right">
+                € 49,990
+              </td>
+              <td class="text-right">
+                <button type="button" rel="tooltip" class="btn btn-success btn-link btn-sm btn-icon   btn-neutral  " data-original-title="Refresh" title="">
+                  <i class="tim-icons icon-refresh-01"></i>
+                </button>
+                <button type="button" rel="tooltip" class="btn btn-danger btn-link btn-sm   btn-neutral  " data-original-title="Delete" title="">
+                  <i class="tim-icons icon-simple-remove"></i>
+                </button>
+              </td>
+            </tr>
+            <tr>
+              <td class="text-center">
+                <div class="photo">
+                  <img src="https://black-dashboard-pro-laravel.creative-tim.com/black/img/robi.jpg" alt="Table image">
+                </div>
+              </td>
+              <td>
+                Paul Dickens
+              </td>
+              <td>
+                Communication
+              </td>
+              <td class="text-center">
+                2015
+              </td>
+              <td class="text-right">
+                € 69,201
+              </td>
+              <td class="text-right">
+                <button type="button" rel="tooltip" class="btn btn-success btn-link btn-sm btn-icon   btn-neutral  " data-original-title="Refresh" title="">
+                  <i class="tim-icons icon-refresh-01"></i>
+                </button>
+                <button type="button" rel="tooltip" class="btn btn-danger btn-link btn-sm   btn-neutral  " data-original-title="Delete" title="">
+                  <i class="tim-icons icon-simple-remove"></i>
+                </button>
+              </td>
+            </tr>
+            <tr>
+              <td class="text-center">
+                <div class="photo">
+                  <img src="https://black-dashboard-pro-laravel.creative-tim.com/black/img/emilyz.jpg" alt="Table image">
+                </div>
+              </td>
+              <td>
+                Manuel Rico
+              </td>
+              <td>
+                Manager
+              </td>
+              <td class="text-center">
+                2012
+              </td>
+              <td class="text-right">
+                € 99,201
+              </td>
+              <td class="text-right">
+                <button type="button" rel="tooltip" class="btn btn-success btn-link btn-sm btn-icon   btn-neutral  " data-original-title="Refresh" title="">
+                  <i class="tim-icons icon-refresh-01"></i>
+                </button>
+                <button type="button" rel="tooltip" class="btn btn-danger btn-link btn-sm   btn-neutral  " data-original-title="Delete" title="">
+                  <i class="tim-icons icon-simple-remove"></i>
+                </button>
+              </td>
+            </tr>
+          </tbody>
+        </table>
+      <div class="ps__rail-x" style="left: 0px; bottom: 0px;"><div class="ps__thumb-x" tabindex="0" style="left: 0px; width: 0px;"></div></div><div class="ps__rail-y" style="top: 0px; right: 0px;"><div class="ps__thumb-y" tabindex="0" style="top: 0px; height: 0px;"></div></div></div>
+    </div>
+  </div>
 </div>
 
 @endsection
--- resources/views/productos/index.blade.php
+++ resources/views/productos/index.blade.php
@@ -1,40 +1,100 @@
 @extends('layouts.app')
 @section('content')
-
-<div class="container">
-    <table class="table table-bordered table-dark">
-        <thead class="thead-dark">
-            <tr>
-            <th scope="col">#</th>
-            <th scope="col">Nombre</th>
-            <th scope="col">Estilo</th>
-            <th scope="col">Tipo de Area</th>
-            <th scope="col">Precio</th>
-            <th scope="col">Fecha de creación</th>
-            <th scope="col">Opciones</th>
-            </tr>
-        </thead>
-        <tbody>
-        @foreach($products as $product)
-            <tr>
-                <td>{{ $product->id }}</td>
-                <td>{{ $product->nombre }}</td>
-                <td>{{ $product->estilo }}</td>
-                <td>{{ $product->tipo_area }}</td>
-                <td>{{ $product->precio }}</td>
-                <td>{{ $product->created_at }}</td>
-                <td>
-                <form action="{{route('productos.destroy', $product->id)}}" method="POST">
-                <button type="button"><a href="{{route('productos.edit', $product->id)}}"></a></button>
-                @csrf
-                @method('DELETE')
-                <button type="submit">Eliminar</button>
-                </form>
-                </td>
-            </tr>
-        @endforeach
-        </tbody>
-        </table>
-</div>
     
+<div class="col-md-12">
+    <div class="card">
+      <div class="card-header">
+        <div class="tools float-right">
+          <div class="dropdown">
+            <button type="button" class="btn btn-default dropdown-toggle btn-link btn-icon" data-toggle="dropdown">
+              <i class="tim-icons icon-settings-gear-63"></i>
+            </button>
+            <div class="dropdown-menu dropdown-menu-right">
+              <a class="dropdown-item" href="#">Action</a>
+              <a class="dropdown-item" href="#">Another action</a>
+              <a class="dropdown-item" href="#">Something else here</a>
+              <a class="dropdown-item text-danger" href="#">Remove Data</a>
+            </div>
+          </div>
+        </div>
+        <h4 class="card-title">Simple Table</h4>
+      </div>
+      <div class="card-body">
+        <div class="table-responsive ps">
+          <table class="table">
+            <thead class="text-primary">
+              <tr>
+                <th scope="col" class="text-center">
+                  #
+                </th>
+                <th scope="col">
+                    Nombre
+                </th>
+                <th scope="col">
+                    Estilo
+                </th>
+                <th scope="col" class="text-center">
+                    Tipo de Area
+                </th>
+                <th scope="col" class="text-right">
+                    Precio
+                </th>
+                <th scope="col" class="text-right">
+                    Fecha de registro
+                </th>
+                <th scope="col" class="text-right">
+                    Opciones
+                </th>
+              </tr>
+            </thead>
+            <tbody>
+                @foreach($products as $product)
+              <tr>
+                <td class="text-center">
+                    {{ $product->id }}
+                  {{-- <div class="photo">
+                    <img src="https://black-dashboard-pro-laravel.creative-tim.com/black/img/tania.jpg" alt="Table image">
+                  </div> --}}
+                </td>
+                <td>
+                    {{ $product->nombre }}
+                </td>
+                <td>
+                        {{ $product->estilo }}
+                </td>
+                <td class="text-center">
+                        {{ $product->tipo_area }}
+                </td>
+                <td class="text-right">
+                        {{ $product->precio }}
+                </td>
+                <td class="text-right">
+                        {{ $product->created_at }}
+                </td>
+                <td class="text-right">
+                    <form action="{{route('productos.destroy', $product->id)}}" method="POST">
+                      <button type="submit" rel="tooltip" class="btn btn-danger btn-link btn-sm " data-original-title="Delete" title=""><i class="tim-icons icon-pencil"></i></button>
+                        <button type="button" rel="tooltip" class="btn btn-success btn-link btn-sm btn-icon " data-original-title="Refresh" title=""><i class="tim-icons icon-refresh-01"></i><a href="{{route('productos.edit', $product->id)}}"></a></button>
+                        @csrf
+                        @method('DELETE')
+                        <button type="submit" rel="tooltip" class="btn btn-danger btn-link btn-sm " data-original-title="Delete" title=""><i class="tim-icons icon-simple-remove"></i></button>
+                    </form>
+
+                  {{-- <button type="button" rel="tooltip" class="btn btn-success btn-link btn-sm btn-icon " data-original-title="Refresh" title="">
+                    <i class="tim-icons icon-refresh-01"></i>
+                  </button>
+                  <button type="button" rel="tooltip" class="btn btn-danger btn-link btn-sm " data-original-title="Delete" title="">
+                    <i class="tim-icons icon-simple-remove"></i>
+                  </button> --}}
+                </td>
+              </tr>
+              
+              @endforeach
+            </tbody>
+          </table>
+        <div class="ps__rail-x" style="left: 0px; bottom: 0px;"><div class="ps__thumb-x" tabindex="0" style="left: 0px; width: 0px;"></div></div><div class="ps__rail-y" style="top: 0px; right: 0px;"><div class="ps__thumb-y" tabindex="0" style="top: 0px; height: 0px;"></div></div></div>
+      </div>
+    </div>
+  </div>
+
 @endsection
